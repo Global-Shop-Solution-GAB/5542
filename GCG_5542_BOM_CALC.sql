aafb5d68283e7c5c1b72ce6d5e88ad9ad089e544
Service Web <sw@gssmail.com>
2024-03-14 12:22:39 -0500

    Commit changes in C:\Websites\Git\emarlow\5542\GCG_5542_BOM_CALC.sql

---
+++ GCG_5542_BOM_CALC.sql
@@ -0,0 +1,21 @@
+/* ==========================================
+ * TABLE: GCG_5542_BOM_CALC
+ * ========================================== */
+CREATE TABLE "GCG_5542_BOM_CALC"(
+ "BomParent" CHAR(20) NOT NULL ,
+ "PL" CHAR(2),
+ "RmCost" DOUBLE,
+ "LotCost" DOUBLE,
+ "LotFactor" DOUBLE,
+ "TotalCost" DOUBLE,
+ "Lot_1MO" DOUBLE,
+ "Var_1MO" DOUBLE,
+ "Per_1MO" DOUBLE,
+ "Lot_6MO" DOUBLE,
+ "Var_6MO" DOUBLE,
+ "Per_6MO" DOUBLE,
+ "Lot_13MO" DOUBLE,
+ "Var_13MO" DOUBLE,
+ "Per_13MO" DOUBLE);
+
+
